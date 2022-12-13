6d2692477d3b2b4a84139f1e7f92476500724518
Amy Lipscomb <amylipscomb@amys-mbp.myfiosgateway.com>
2022-12-13 08:20:44 -0500

    added owner.js file in models folder; updated code in connection.js file; created an env example file

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,41 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
+let sequelize;
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+      port: 3306 
+    }
+  );
+}
 
 module.exports = sequelize;
+
+
+
+
+//Not sure about the coding below-will ask in tutoring session
+// require('dotenv').config();
+
+// const Sequelize = require('sequelize');
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+// module.exports = sequelize;
