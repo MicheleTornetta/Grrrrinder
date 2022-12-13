41c1bc411299dea72b870b03b1b08852952ea364
unknown <samanthajanedavidson@gmail.com>
2022-12-13 17:38:50 -0500

    Updated schema file

--- db/schema.sql
+++ db/schema.sql
@@ -9,8 +9,8 @@ USE grrrrinder_db;
 CREATE TABLE owner (
     id INT auto_increment NOT NULL, 
     owner_first_name VARCHAR(30) NOT NULL,
-    owner_last_name VARCHAR (30), --leave this as optional? Should we even have this? 
-    owner_location VARCHAR(30) NOT NULL, --just ask for zip code here? 
+    owner_last_name VARCHAR (30), --leave this as optional
+    owner_location VARCHAR(30) NOT NULL, --just ask for zip code here
     username VARCHAR (30) NOT NULL, --email address
     password VARCHAR(30) NOT NULL, --enrypted 
     PRIMARY KEY (id)
@@ -20,23 +20,23 @@ CREATE TABLE dog (
     id INT auto_increment NOT NULL,
     dog_name VARCHAR(30) NOT NULL,
     dog_breed VARCHAR(30) NOT NULL,
-    dog_gender  -- male, female
-    dog_size  -- small, medium, large, extra large 
+    dog_gender VARCHAR(10) NOT NULL, -- male, female
+    dog_size VARCHAR(20) NOT NULL, -- small, medium, large, extra large 
     dog_age DECIMAL NOT NUll, 
-    dog_vaccinations --yes/no 
-    dog_neuter_spayed --yes/no 
+    dog_vaccinations VARCHAR(5) NOT NULL,--yes/no 
+    dog_neuter_spayed VARCHAR(5) NOT NULL,--yes/no 
     dog_temperment VARCHAR(200) NOT NULL,
     dog_notes VARCHAR(200),
-    dog_picture --I'm not exactly sure how to do this yet. Tutor suggested Firebase db but I need to research it.  
+    dog_picture VARBINARY(max), --looking into Cloudinary
     PRIMARY KEY (id)
 );
 
 CREATE TABLE meetup (
     id INT auto_increment NOT NULL,
     preferred_dates DATE NOT NULL, -- list multiple selections 
-    preferred_times VARCHAR (30), --time of day selection i.e. morning, afternoon, etc.?
-    preferred_location VARCHAR (30), --based on zip again? 
+    preferred_times VARCHAR (30), --time of day selection i.e. morning, afternoon, etc.
+    preferred_location VARCHAR (30), --based on zip again
     PRIMARY KEY (id)
 );
 
--- link to instagram or email to connect people 
+
