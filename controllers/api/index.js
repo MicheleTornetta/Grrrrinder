01652712ad80911612bb81d56e7fd8c42c7696e9
unknown <samanthajanedavidson@gmail.com>
2022-12-15 17:34:53 -0500

    Updated index route file to remove meetup

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const dogRoutes = require('./dogRoutes');
-// const meetupRoutes = require('./meetupRoutes');
-// const ownerRoutes = require('./ownerRoutes');
+const ownerRoutes = require('./ownerRoutes');
 
 router.use('/dogs', dogRoutes);
-// router.use('/meetups', meetupRoutes);
-// router.use('/owners', ownerRoutes);
+router.use('/owners', ownerRoutes);
 
 module.exports = router;
