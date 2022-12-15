ff1eba3f66c3b6b0c65eff0feae796318c8a41ea
unknown <samanthajanedavidson@gmail.com>
2022-12-14 19:45:00 -0500

    I dunno I think I just made some notes; no major changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const dogRoutes = require('./dogRoutes');
-const meetupRoutes = require('./meetupRoutes');
-const ownerRoutes = require('./ownerRoutes');
+// const meetupRoutes = require('./meetupRoutes');
+// const ownerRoutes = require('./ownerRoutes');
 
 router.use('/dogs', dogRoutes);
-router.use('/meetups', meetupRoutes);
-router.use('/owners', ownerRoutes);
+// router.use('/meetups', meetupRoutes);
+// router.use('/owners', ownerRoutes);
 
 module.exports = router;
---
+++ controllers/api/ownerRoutes.js
@@ -0,0 +1,5 @@
+// CREATE 
+
+// DELETE 
+
+//INclued all of the password hashing and such here 
