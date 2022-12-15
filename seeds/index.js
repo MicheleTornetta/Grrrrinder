da69f7cab97324af59aa91a91d8e0873c2cf2d7d
unknown <samanthajanedavidson@gmail.com>
2022-12-15 16:33:45 -0500

    Updated index file to remove meetup seed info

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,4 @@
 const seedDogs = require('./dogSeedData');
-const seedMeetup = require('./meetupSeedData');
 
 const sequelize = require('../config/connection');
 
@@ -8,9 +7,6 @@ const seedAll = async () => {
   console.log('\n----- DATABASE SYNCED -----\n');
   await seedDogs();
   console.log('\n----- DOGS SEEDED -----\n');
-
-  await seedMeetup();
-  console.log('\n----- MEETUPS SEEDED -----\n');
 
   process.exit(0);
 };
