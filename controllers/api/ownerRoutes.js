50572f69a870ab73acf309c555fc56a338dc0981
unknown <samanthajanedavidson@gmail.com>
2022-12-15 17:34:11 -0500

    Added owner route; deleted meetup route; updated dog route

--- controllers/api/ownerRoutes.js
+++ controllers/api/ownerRoutes.js
@@ -1,5 +1,37 @@
-// CREATE 
+const router = require('express').Router();
+const { Dog, Owner } = require('../../models');
 
-// DELETE 
 
-// Inclued all of the password hashing and such here 
+// CREATE owner account
+router.post('/', async (req, res) => {
+    try {
+        const ownerData = await Owner.create(req.body) /
+
+        res.status(200).json(ownerData);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// DELETE owner account
+router.delete('/:id', async (req, res) => {
+    try {
+        const ownerData = await Owner.destroy({
+        where: {
+            id: req.params.id,
+        }
+    })
+        if (!ownerData) {
+            res.status(404).json({ messsage: "No account with this login information"});
+            return;
+        }
+        res.status(200).json(ownerData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
+
