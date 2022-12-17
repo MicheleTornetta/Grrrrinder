8f27ec17c39f3792e7012a53d87065533d934b17
CassNoisette <>
2022-12-17 15:42:32 -0500

    added engine and path to server

--- controllers/auth/authentication.js
+++ controllers/auth/authentication.js
@@ -1,8 +1,8 @@
-// function checkAuth (req, res, next) {
-//     if (req.session.user) 
-//         next();
-//     else
-//         res.status(400).json({err: 'You must be logged in to perform this action.'});
-// }
+function checkAuth (req, res, next) {
+    if (req.session.user) 
+        next();
+    else
+        res.status(400).json({err: 'You must be logged in to perform this action.'});
+}
 
-// module.exports = checkAuth;
+module.exports = checkAuth;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require ('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -9,9 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 const { Dog, Owner } = require('./models');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(routes);
