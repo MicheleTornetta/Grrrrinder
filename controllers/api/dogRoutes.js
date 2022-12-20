dd779dad2327d44667713e29cab96368dfcbe3a4
Max Spangle <nataliespangle@natalies-air.myfiosgateway.com>
2022-12-19 21:00:25 -0500

    fixed javascript link

--- controllers/api/dogRoutes.js
+++ controllers/api/dogRoutes.js
@@ -29,34 +29,34 @@ router.get('/', async (req, res) => {
     }
 });
 
-// // GET dogs based on specific search criteria
-router.get('/:one/:two:/:three/:four/:five/:six/:seven/:eight/:nine', async (req, res) => {
-    try {
-        const dogData = await Dog.findAll({
-            where: { //need two different versions of this per Anthony? 
-                [Op.or]: [ //and is not working here but we had it working with an or at one point but now that isn't working either :()
-                    { dog_gender: req.params.one },
-                    { dog_size: req.params.two},
-                    { dog_age: req.params.three },
-                    { dog_vaccinations: req.params.four },
-                    { dog_neuter_spayed: req.params.five },
-                    { dog_temperment: req.params.six },
-                    { preferred_days: req.params.seven },
-                    { preferred_timeofday: req.params.eight },
-                    { preferred_location: req.params.nine },
-                ]
-            }
-        })
+// // // GET dogs based on specific search criteria
+// router.get('/:one/:two:/:three/:four/:five/:six/:seven/:eight/:nine', async (req, res) => {
+//     try {
+//         const dogData = await Dog.findAll({
+//             where: { //need two different versions of this per Anthony? 
+//                 [Op.or]: [ //and is not working here but we had it working with an or at one point but now that isn't working either :()
+//                     { dog_gender: req.params.one },
+//                     { dog_size: req.params.two},
+//                     { dog_age: req.params.three },
+//                     { dog_vaccinations: req.params.four },
+//                     { dog_neuter_spayed: req.params.five },
+//                     { dog_temperment: req.params.six },
+//                     { preferred_days: req.params.seven },
+//                     { preferred_timeofday: req.params.eight },
+//                     { preferred_location: req.params.nine },
+//                 ]
+//             }
+//         })
 
-        if (!dogData) {
-            res.status(404).json({ message: "No dogs found meeting that search criteria" });
-            return;
-        }
-        res.status(200).json(dogData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+//         if (!dogData) {
+//             res.status(404).json({ message: "No dogs found meeting that search criteria" });
+//             return;
+//         }
+//         res.status(200).json(dogData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 // CREATE new dog profile 
 router.post('/', checkAuth, async (req, res) => {
